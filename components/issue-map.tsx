7fd0c73663602f74e23604fbef5662db1a9f1d81
erkhembilegsainbayar <erkhembileg.sainbayar@gmail.com>
2026-09-12 19:47:13 +0800

    Create issue-map.tsx

---
+++ components/issue-map.tsx
@@ -0,0 +1,3 @@
+export default function IssueMap() {
+  return <div style={{ height: '400px', background: '#eaeaea', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Map Container Layout Place Holder</div>;
+}
